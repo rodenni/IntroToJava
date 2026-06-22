cf2b76d6005458d92b489d81de23ba2b48b29ace
rodenni <denniserodney@gmail.com>
2026-06-22 10:39:13 -0400

    Exercise01_01.java

---
+++ src/Exercise01_01.java
@@ -0,0 +1,6 @@
+public class Exercise01_01 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java\nWelcome to Computer Science\nProgramming is fun");
+    }
+
+}
